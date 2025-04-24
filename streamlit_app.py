21f85e6279440ddda12c954fb91928a02bc062ba
adikarap <sultanadikaraputra@gmail.com>
2025-04-24 14:50:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Sultan Adikara Putra")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Hallo guys nama aku Sultan Adikara Putra aku sekola di SMAN 20 Bandung."
 )
