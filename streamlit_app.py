5c55656b453e06a11be322a72771af8e8dad6220
adikarap <sultanadikaraputra@gmail.com>
2025-04-24 14:51:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Sultan Adikara Putra")
 st.write(
-    "Hallo guys nama aku Sultan Adikara Putra aku sekola di SMAN 20 Bandung."
+    "Hallo guys, nama aku Sultan Adikara Putra aku sekola di SMAN 20 Bandung."
 )
